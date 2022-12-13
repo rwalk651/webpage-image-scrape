2fbcd49e27ac29731d06694dae6c6c4d2939dccb
rwalk651 <li5823od@go.minnstate.edu>
2022-12-13 03:08:03 -0500

    qol fixes

--- get_eso_official_links.py
+++ get_eso_official_links.py
@@ -3,7 +3,8 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from bs4 import *
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
@@ -34,8 +35,11 @@ def get_image_urls(sleep_between_interactions: float = 5):
         year.send_keys('1980')
         time.sleep(1)
 
+        button = driver.find_element(By.CSS_SELECTOR, 'button.btn-gate')
+        button.click()
+
     image_urls = set()
-    # fetch 5 urls at a time, program does not remember place
+    # fetch 2 urls at a time, program does not remember place
     link_max = 2
     image_count = 0
     results_start = 0
@@ -43,20 +47,31 @@ def get_image_urls(sleep_between_interactions: float = 5):
         scroll_to_end(driver)
 
         # get image thumbnail results
+        # thumbnail_results = driver.find_elements(By.CSS_SELECTOR, 'img[class="img-responsive"]')
         thumbnail_results = driver.find_elements(By.CSS_SELECTOR, 'img.img-responsive')
         number_results = len(thumbnail_results)
 
         print(f'Found: {number_results} results. Extracting links from {results_start}:{number_results}.')
 
-        for thumb in thumbnail_results[results_start:number_results]:
+        for thumb in thumbnail_results:
 
             try:
-                thumb.click()
+                WebDriverWait(driver, sleep_between_interactions).until(EC.element_to_be_clickable(thumb)).click()
                 time.sleep(sleep_between_interactions)
             except Exception:
-                continue
+                print('unable to click')
+                time.sleep(sleep_between_interactions)
+                break
 
-            images = driver.find_elements(By.XPATH, '// a[contains(text(), "1920x1080")]')
+            # images = driver.find_elements(By.XPATH, '//a[contains(text(), "1920x1080")]')
+            # images = driver.find_elements(By.CSS_SELECTOR, 'a.gl-link-checked')
+            if driver.find_element(By.LINK_TEXT, '1920x1080'):
+                images = driver.find_elements(By.LINK_TEXT, '1920x1080')
+            elif driver.find_element(By.PARTIAL_LINK_TEXT, '1920'):
+                images = driver.find_elements(By.PARTIAL_LINK_TEXT, '1920')
+            else:
+                print('No Element')
+                break
 
             for img in images:
                 # extract image urls
